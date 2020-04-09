29ec9cb8bfc7190d33b2ce9ce9e0e57c96b21f69
Connor Dufault <mx8434an@go.minnstate.edu>
2020-04-09 16:07:32 -0500

    made WaterBill classy

---
+++ WaterBill/WaterBillAnalysis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterBill
+{
+    class WaterBillAnalysis
+    {
+        public double SumQuarterlyBills(List<double> quarters)
+        {
+            // add up all of the values
+            double total = 0;
+            foreach (double value in quarters)
+            {
+                total += value;
+            }
+            return total;
+        }
+
+        public double AvgQuarterlyBill(List<double> quarters)
+        {
+            double total = SumQuarterlyBills(quarters);
+            return total / quarters.Count;
+        }
+    }
+}
